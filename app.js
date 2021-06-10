aedd8c82a51b07decf20a7227fdb0d69861b2cd4
dvlprwchoi <dvlpr.wchoi@gmail.com>
2021-06-09 21:54:42 -0500

    ADD won class, CSS transition

--- app.js
+++ app.js
@@ -47,11 +47,13 @@ const mark = (event) => {
 const resetGame = () => {
     for (const cellGrid of allCellGrids) {
         cellGrid.classList.remove("black");
-        cellGrid.classList.remove("white");    
+        cellGrid.classList.remove("white");
+        cellGrid.classList.remove("won");    
         // console.log(cellGrid)
     }
     gameStatus = true;
     whiteStoneNext = true;
+    winner = null;
 }
 
 
@@ -98,50 +100,110 @@ const win = () => {
     // console.log(r1c4, r2c4, r3c4, r4c4, r5c4);
     
     if (r1c1 && r1c1 === r1c2 && r1c1 === r1c3 && r1c1 === r1c4 && r1c1 === r1c5) {
+        allCellGrids[0].classList.add("won");
+        allCellGrids[1].classList.add("won");
+        allCellGrids[2].classList.add("won");
+        allCellGrids[3].classList.add("won");
+        allCellGrids[4].classList.add("won");
         gameStatus = false;
         winner = r1c1;
         console.log(winner);
     } else if (r2c1 && r2c1 === r2c2 && r2c1 === r2c3 && r2c1 === r2c4 && r2c1 === r2c5) {
+        allCellGrids[5].classList.add("won");
+        allCellGrids[6].classList.add("won");
+        allCellGrids[7].classList.add("won");
+        allCellGrids[8].classList.add("won");
+        allCellGrids[9].classList.add("won");
         gameStatus = false;
         winner = r2c1;
         console.log(winner);
     } else if (r3c1 && r3c1 === r3c2 && r3c1 === r3c3 && r3c1 === r3c4 && r3c1 === r3c5) {
+        allCellGrids[10].classList.add("won");
+        allCellGrids[11].classList.add("won");
+        allCellGrids[12].classList.add("won");
+        allCellGrids[13].classList.add("won");
+        allCellGrids[14].classList.add("won");
         gameStatus = false;
         winner = r3c1;
         console.log(winner);
     } else if (r4c1 && r4c1 === r4c2 && r4c1 === r4c3 && r4c1 === r4c4 && r4c1 === r4c5) {
+        allCellGrids[15].classList.add("won");
+        allCellGrids[16].classList.add("won");
+        allCellGrids[17].classList.add("won");
+        allCellGrids[18].classList.add("won");
+        allCellGrids[19].classList.add("won");
         gameStatus = false;
         winner = r4c1;
         console.log(winner);
     } else if (r5c1 && r5c1 === r5c2 && r5c1 === r5c3 && r5c1 === r5c4 && r5c1 === r5c5) {
+        allCellGrids[20].classList.add("won");
+        allCellGrids[21].classList.add("won");
+        allCellGrids[22].classList.add("won");
+        allCellGrids[23].classList.add("won");
+        allCellGrids[24].classList.add("won");
         gameStatus = false;
         winner = r5c1;
         console.log(winner);
     } else if (r1c1 && r1c1 === r2c1 && r1c1 === r3c1 && r1c1 === r4c1 && r1c1 === r5c1) {
+        allCellGrids[0].classList.add("won");
+        allCellGrids[5].classList.add("won");
+        allCellGrids[10].classList.add("won");
+        allCellGrids[15].classList.add("won");
+        allCellGrids[20].classList.add("won");
         gameStatus = false;
         winner = r1c1;
         console.log(winner);
     } else if (r1c2 && r1c2 === r2c2 && r1c2 === r3c2 && r1c2 === r4c2 && r1c2 === r5c2) {
+        allCellGrids[1].classList.add("won");
+        allCellGrids[6].classList.add("won");
+        allCellGrids[11].classList.add("won");
+        allCellGrids[16].classList.add("won");
+        allCellGrids[21].classList.add("won");
         gameStatus = false;
         winner = r1c2;
         console.log(winner);
     } else if (r1c3 && r1c3 === r2c3 && r1c3 === r3c3 && r1c3 === r4c3 && r1c3 === r5c3) {
+        allCellGrids[2].classList.add("won");
+        allCellGrids[7].classList.add("won");
+        allCellGrids[12].classList.add("won");
+        allCellGrids[17].classList.add("won");
+        allCellGrids[22].classList.add("won");
         gameStatus = false;
         winner = r1c3;
         console.log(winner);
     } else if (r1c4 && r1c4 === r2c4 && r1c4 === r3c4 && r1c4 === r4c4 && r1c4 === r5c4) {
+        allCellGrids[3].classList.add("won");
+        allCellGrids[8].classList.add("won");
+        allCellGrids[13].classList.add("won");
+        allCellGrids[18].classList.add("won");
+        allCellGrids[23].classList.add("won");
         gameStatus = false;
         winner = r1c4;
         console.log(winner);
     } else if (r1c5 && r1c5 === r2c5 && r1c5 === r3c5 && r1c5 === r4c5 && r1c5 === r5c5) {
+        allCellGrids[4].classList.add("won");
+        allCellGrids[9].classList.add("won");
+        allCellGrids[14].classList.add("won");
+        allCellGrids[19].classList.add("won");
+        allCellGrids[24].classList.add("won");
         gameStatus = false;
         winner = r1c5;
         console.log(winner);
     } else if (r1c5 && r1c5 === r2c4 && r1c5 === r3c3 && r1c5 === r4c2 && r1c5 === r5c1) {
+        allCellGrids[4].classList.add("won");
+        allCellGrids[8].classList.add("won");
+        allCellGrids[12].classList.add("won");
+        allCellGrids[16].classList.add("won");
+        allCellGrids[20].classList.add("won");
         gameStatus = false;
         winner = r1c5;
         console.log(winner);
     } else if (r1c1 && r1c1 === r2c2 && r1c1 === r3c3 && r1c1 === r4c4 && r1c1 === r5c5) {
+        allCellGrids[0].classList.add("won");
+        allCellGrids[6].classList.add("won");
+        allCellGrids[12].classList.add("won");
+        allCellGrids[18].classList.add("won");
+        allCellGrids[24].classList.add("won");
         gameStatus = false;
         winner = r1c1;
         console.log(winner);
